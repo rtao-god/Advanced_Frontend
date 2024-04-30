8738320d573963046d9678467f64461a128d3632
Rtao <prostodimka519@gmail.com>
2024-04-30 03:54:47 +0300

    Added api for auth

---
+++ src/shared/api/getUser.ts
@@ -0,0 +1,14 @@
+import { AxiosResponse } from 'axios'
+
+import { IUserData } from '@/shared/types/user.interface'
+import { instance } from '../config/axiosInstance/axiosInstance'
+
+export async function getUser(token: string): Promise<AxiosResponse<IUserData>> {
+  const res = await instance.get('/users-detail/', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  return res
+}
