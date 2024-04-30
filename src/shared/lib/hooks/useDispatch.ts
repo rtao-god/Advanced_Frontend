8108715f70de7fefe6122f8bf99bc0f9eca8f802
Rtao <prostodimka519@gmail.com>
2024-04-30 03:55:42 +0300

    Added hooks

---
+++ src/shared/lib/hooks/useDispatch.ts
@@ -0,0 +1,7 @@
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/app/providers/StoreProvider/config/store'
+
+export const useAppDispatch = () => {
+  const dispatch = useDispatch<AppDispatch>()
+  return dispatch
+}
