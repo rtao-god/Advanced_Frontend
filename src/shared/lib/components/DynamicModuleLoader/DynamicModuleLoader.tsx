f1da04c167f1b19d1325eaeb080196aba0bf8e1a
Rtao <prostodimka519@gmail.com>
2024-05-01 05:23:33 +0300

    Deleted useAppDispatch

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,9 +1,8 @@
 import { ReduxStoreWithManager } from '@/app/providers/StoreProvider'
 import { ReactNode, useEffect } from 'react'
-import { useAppDispatch } from '../../hooks/useDispatch'
 import { StateSchemaKey } from '@/app/providers/StoreProvider/config/StateSchema'
 import { Reducer } from '@reduxjs/toolkit'
-import { useStore } from 'react-redux'
+import { useDispatch, useStore } from 'react-redux'
 
 export type ReducersList = {
     [name in StateSchemaKey]?: Reducer
@@ -19,7 +18,7 @@ interface DynamicModuleLoaderProps {
 
 export default function DynamicModuleLoader({ children, reducers, removeAfterUnmount = false }: DynamicModuleLoaderProps) {
     const store = useStore() as ReduxStoreWithManager
-    const dispatch = useAppDispatch()
+    const dispatch = useDispatch()
 
     useEffect(() => {
         (Object.entries(reducers) as ReducersListEntry[]).forEach(([name, reducer]) => {
