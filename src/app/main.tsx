a166b34eec0256bab811b495d80d657e23385f6d
Rtao <prostodimka519@gmail.com>
2024-05-02 06:29:15 +0300

    Added Mirage

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,15 +5,25 @@ import { ThemeProvider } from './providers/ThemeProvider'
 import '../shared/config/i18n/i18n'
 import { ErrorBoundary } from './providers/ErrorBoundary'
 import { StoreProvider } from './providers/StoreProvider'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { makeServer } from '@/mirage/server'
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StoreProvider>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StoreProvider>
 )
