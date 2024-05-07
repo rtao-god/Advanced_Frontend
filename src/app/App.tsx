dc482807cf21a2f13cb1eec0afb73d5d9f5a7deb
Rtao <prostodimka519@gmail.com>
2024-05-08 00:37:12 +0300

    Added AnimateOpacity

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,22 @@
 import './index.sass'
 import { AppRouter } from './providers/router'
-import useTheme from '@/shared/lib/hooks/useTheme'
 import classNames from '@/shared/lib/helpers/classNames'
 import { PSuspense } from './providers/Suspense'
-import { AnimatePresence, motion } from 'framer-motion'
+import { ThemeContext } from './providers/ThemeProvider/lib/ThemeContext'
+import { useTheme } from '@/shared/lib/hooks/useTheme'
+import AnimateOpacity from '@/shared/ui/animations/AnimateOpacity/AnimateOpacity'
 
 function App() {
   const { theme } = useTheme()
 
+  console.log(ThemeContext)
+
   return (
     <div className={classNames('app', {}, [theme])}>
       <PSuspense>
-        <AnimatePresence initial={true}>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <AppRouter />
-          </motion.div>
-        </AnimatePresence>
+        <AnimateOpacity>
+          <AppRouter />
+        </AnimateOpacity>
       </PSuspense>
     </div>
   )
