4ca6e7702d76e37e6263e6f231eff12ba5e01671
rtao-god <prostodimka519@gmail.com>
2024-05-14 04:48:13 +0300

    added @import ./global, then deleted unnecessary imports in the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,11 @@ import './index.sass'
 import { AppRouter } from './providers/router'
 import classNames from '@/shared/lib/helpers/classNames'
 import { PSuspense } from './providers/Suspense'
-import { ThemeContext } from './providers/ThemeProvider/lib/ThemeContext'
 import { useTheme } from '@/shared/lib/hooks/useTheme'
 import AnimateOpacity from '@/shared/ui/animations/AnimateOpacity/AnimateOpacity'
 
 function App() {
   const { theme } = useTheme()
-
-  console.log(ThemeContext)
 
   return (
     <div className={classNames('app', {}, [theme])}>
