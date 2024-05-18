bf903b49e04334635e329627248834c5b3561b7b
rtao-god <prostodimka519@gmail.com>
2024-05-18 13:29:21 +0300

    Testing tree generation (added a mock TestUtilsdFolder folder)

--- post-commit.ps1
+++ post-commit.ps1
@@ -1,12 +1,5 @@
-npm run gs
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "Tree structure generated successfully."
-} else {
-    Write-Host "Failed to generate tree structure."
-    exit 1
-}
+struct src -o src-structure.tree
 
 git add src-structure.tree
+
 git commit --allow-empty -m "Update structure tree"
-git push
