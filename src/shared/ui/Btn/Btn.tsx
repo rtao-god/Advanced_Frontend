c35b85ed4532daec826342b75fe2e2394385497b
rtao-god <prostodimka519@gmail.com>
2024-05-19 02:08:17 +0300

    Rewrote the button using AnimateComponent

--- src/shared/ui/Btn/Btn.tsx
+++ src/shared/ui/Btn/Btn.tsx
@@ -1,15 +1,16 @@
-import classNames from '@/shared/lib/helpers/classNames';
-import cls from './Btn.module.sass';
-import { useTheme } from '@/shared/lib/hooks/useTheme';
-import BtnProps from './types';
-import { Theme } from '@/app/providers/ThemeProvider/lib/ThemeContext';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion'
+import { useTheme } from '@/shared/lib/hooks/useTheme'
+import classNames from '@/shared/lib/helpers/classNames'
+import cls from './Btn.module.sass'
+import BtnProps from './types'
+import { Theme } from '@/app/providers/ThemeProvider/lib/ThemeContext'
+import AnimateComponent from '../animations/AnimateComponent/AnimateComponent'
 
 export default function Btn({
   color,
-  width = "100%",
+  width = '100%',
   children,
-  onClick = () => "",
+  onClick = () => '',
   br,
   padding,
   disabled,
@@ -22,11 +23,12 @@ export default function Btn({
   className,
   size,
 }: BtnProps) {
-  const { theme } = useTheme();
-  const themeColor = theme === Theme.DARK ? 'green' : 'blue';
+  const { theme } = useTheme()
+  const themeColor = theme === Theme.DARK ? 'green' : 'blue'
 
   return (
-    <motion.button
+    <AnimateComponent
+      Component={motion.button}
       className={classNames(cls.Btn, { [cls[themeColor]]: true }, [className || ''])}
       type={type}
       style={{
@@ -37,12 +39,11 @@ export default function Btn({
         height,
         minWidth: minW,
       }}
-      whileHover={{ boxShadow: "0px 0px 20px rgba(0, 0, 255, 0.7)" }}
-      whileTap={{ boxShadow: "0px 0px 15px rgba(255, 0, 0, 0.7)" }}
+      whileHover={{ boxShadow: '0px 0px 20px rgba(0, 0, 255, 0.7)' }}
+      whileTap={{ boxShadow: '0px 0px 15px rgba(255, 0, 0, 0.7)' }}
       disabled={disabled}
-      onClick={() => onClick()}
-    >
+      onClick={() => onClick()}>
       {children}
-    </motion.button>
-  );
-};
+    </AnimateComponent>
+  )
+}
