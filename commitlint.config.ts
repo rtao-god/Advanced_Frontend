864081829263eda6cad683a3feb280fc59f78084
rtao-god <prostodimka519@gmail.com>
2024-05-25 04:33:38 +0300

    docs(commitlint): add comments to commit types for better clarity
    - Added comments explaining each commit type for improved readability and understanding.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,11 +1,20 @@
-import { UserConfig } from '@commitlint/types'
+import { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test']],
+    'type-enum': [2, 'always', [
+      'feat', // Introducing new features
+      'fix', // Bug fixes
+      'chore', // Routine tasks and maintenance
+      'docs', // Documentation changes
+      'style', // Code style changes (formatting, missing semi-colons, etc.)
+      'refactor', // Code refactoring without changing functionality
+      'perf', // Performance improvements
+      'test' // Adding or updating tests
+    ]],
     'subject-case': [2, 'never', ['sentence-case']],
   },
-}
+};
 
-export default Configuration
+export default Configuration;
