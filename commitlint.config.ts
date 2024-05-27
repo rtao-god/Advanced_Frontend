b1e26d7edf9b890399a90c296fe7f89a6d8bbf48
rtao-god <prostodimka519@gmail.com>
2024-05-27 11:40:28 +0300

    docs(commitlint): added rules for commits
    - Added the header-max-length rule
    - Added the subject-empty rule
    - Added the type-empty rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,20 +1,27 @@
-import { UserConfig } from '@commitlint/types';
+import { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', [
-      'feat', // Introducing new features
-      'fix', // Bug fixes
-      'chore', // Routine tasks and maintenance
-      'docs', // Documentation changes
-      'style', // Code style changes (formatting, missing semi-colons, etc.)
-      'refactor', // Code refactoring without changing functionality
-      'perf', // Performance improvements
-      'test' // Adding or updating tests
-    ]],
-    'subject-case': [2, 'never', ['sentence-case']],
-  },
-};
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat', // Introducing new features
+                'fix', // Bug fixes
+                'chore', // Routine tasks and maintenance
+                'docs', // Documentation changes
+                'style', // Code style changes (formatting, missing semi-colons, etc.)
+                'refactor', // Code refactoring without changing functionality
+                'perf', // Performance improvements
+                'test' // Adding or updating tests
+            ]
+        ],
+        'subject-case': [2, 'never', ['sentence-case']],
+        'header-max-length': [2, 'always', 100],
+        'subject-empty': [2, 'never'],
+        'type-empty': [2, 'never']
+    }
+}
 
-export default Configuration;
+export default Configuration
