43cb62f613aab7720536c03ed50fe824041aaddf
rtao-god <prostodimka519@gmail.com>
2024-05-27 11:29:38 +0300

    chore(vite/css): add SCSS support in Vite preprocessor options
    - Added SCSS support in the Vite configuration

--- config/vite/css.ts
+++ config/vite/css.ts
@@ -3,5 +3,8 @@ export const css = {
       sass: {
         additionalData: `@import '@/app/styles/include.sass'\n`,
       },
+      scss: {
+        additionalData: `@import '@/app/styles/include.sass';\n`,
+      },
     },
   };
