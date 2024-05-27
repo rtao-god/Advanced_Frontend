9d5242159079a39bd700a974e132bbe856b7cfc2
rtao-god <prostodimka519@gmail.com>
2024-05-27 11:08:10 +0300

    docs(eslint): configured version 9 of eslint with plugins and command
    - Added configuration for ESLint version 9
    - Installed necessary plugins for ESLint:
    react-refresh and typescript-eslint
    - Configured ESLint rules to match project requirements
    - Set up the ESLint command for proper linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,56 +1,84 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint';
-import eslintReact from 'eslint-plugin-react';
-import eslintReactHooks from 'eslint-plugin-react-hooks';
-import prettierPlugin from 'prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintImport from 'eslint-plugin-import';
-import eslintJsxA11y from 'eslint-plugin-jsx-a11y';
-import eslintNode from 'eslint-plugin-node';
-import eslintPromise from 'eslint-plugin-promise';
-import eslintSecurity from 'eslint-plugin-security';
-import eslintStylelint from 'eslint-plugin-stylelint';
-import eslintUnusedImports from 'eslint-plugin-unused-imports';
-import globals from 'globals';
+import tseslint from 'typescript-eslint'
+import eslint from '@eslint/js'
+import eslintReact from 'eslint-plugin-react'
+import eslintReactHooks from 'eslint-plugin-react-hooks'
+import eslintReactRefresh from 'eslint-plugin-react-refresh'
+import eslintImport from 'eslint-plugin-import'
+import eslintJsxA11y from 'eslint-plugin-jsx-a11y'
+import eslintNode from 'eslint-plugin-node'
+import eslintPromise from 'eslint-plugin-promise'
+import eslintSecurity from 'eslint-plugin-security'
+import eslintStylelint from 'eslint-plugin-stylelint'
+import eslintUnusedImports from 'eslint-plugin-unused-imports'
+import globals from 'globals'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default tseslint.config(
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    ignores: ['node-modules', 'dist', '.vscode', '.husky', 'coverage', 'analyse.html'],
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2020,
-      },
-      parserOptions: {
-        project: ['tsconfig.json'],
-        tsconfigRootDir: __dirname, 
-      },
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    prettierConfig,
+    {
+        ignores: ['node_modules', 'dist', '.vscode', '.husky', 'coverage', 'analyse.html']
     },
-  },
-  {
-    files: ["src/**/*.{ts,tsx}"],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'react': eslintReact,
-      'react-hooks': eslintReactHooks,
-      prettier: prettierPlugin,
-      // 'import': eslintImport,
-      // 'jsx-a11y': eslintJsxA11y,
-      // 'node': eslintNode,
-      // 'promise': eslintPromise,
-      // 'security': eslintSecurity,
-      // 'stylelint': eslintStylelint,
-      // 'unused-imports': eslintUnusedImports,
-    },
-    rules: {
-      ...eslintConfigPrettier.rules,
-      'prefer-const': 'error',
-    },
-  },
+    {
+        files: ['src/**/*.{ts,tsx}'],
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+                extraFileExtensions: ['.tsx, .jsx']
+            },
+            parser: tseslint.parser,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2021
+            }
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            react: eslintReact,
+            'react-hooks': eslintReactHooks,
+            'react-refresh': eslintReactRefresh,
+            prettier: prettierPlugin,
+            import: eslintImport,
+            'jsx-a11y': eslintJsxA11y,
+            node: eslintNode,
+            promise: eslintPromise,
+            security: eslintSecurity,
+            stylelint: eslintStylelint,
+            'unused-imports': eslintUnusedImports
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'prefer-const': 'error',
+            'unused-imports/no-unused-imports': 'error',
+            'import/no-unresolved': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+            ],
+            'react/prop-types': 'off'
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            },
+            'import/parsers': {
+                '@typescript-eslint/parser': ['.ts', '.tsx'],
+                typescript: true,
+                node: true
+            },
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx']
+                },
+                typescript: {
+                    alwaysTryTypes: true
+                }
+            }
+        }
+    }
 )
