fffc5f1ee6117bc5485b65a9646a6cb6a69f7ec6
rtao-god <prostodimka519@gmail.com>
2024-05-28 12:23:56 +0300

    fix(waveAnimation): rewrote from sass to scss
    - Rewrote from sass to scss

--- src/shared/ui/animations/WaveAnimate/WaveAnimate.tsx
+++ src/shared/ui/animations/WaveAnimate/WaveAnimate.tsx
@@ -1,25 +1,25 @@
 import classNames from '@/shared/lib/helpers/classNames'
 import WaveAnimateProps from './types'
-import cls from './WaveAnimate.module.sass'
+import cls from './WaveAnimate.module.scss'
 
 export default function WaveAnimate({ className }: WaveAnimateProps) {
-  return (
-    <div className={classNames(cls.Wave_animate, {}, [className || ''])}>
-      <div className={cls.waves}>
-        <div className={`${cls.wave_container} ${cls.wave_animation}`}>
-          <div className={`${cls.wave_layer} ${cls.bg_top}`}>
-            <div className={cls.wave_top}></div>
-          </div>
+    return (
+        <div className={classNames(cls.Wave_animate, {}, [className ?? ''])}>
+            <div className={cls.waves}>
+                <div className={`${cls.wave_container} ${cls.wave_animation}`}>
+                    <div className={`${cls.wave_layer} ${cls.bg_top}`}>
+                        <div className={cls.wave_top}></div>
+                    </div>
 
-          <div className={`${cls.wave_layer} ${cls.bg_middle}`}>
-            <div className={cls.wave_middle}></div>
-          </div>
+                    <div className={`${cls.wave_layer} ${cls.bg_middle}`}>
+                        <div className={cls.wave_middle}></div>
+                    </div>
 
-          <div className={`${cls.wave_layer} ${cls.bg_bottom}`}>
-            <div className={cls.wave_bottom}></div>
-          </div>
+                    <div className={`${cls.wave_layer} ${cls.bg_bottom}`}>
+                        <div className={cls.wave_bottom}></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
