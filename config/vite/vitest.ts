3b70fab5e88cda7f9963b3cbff9c7f0149ca9a40
rtao-god <prostodimka519@gmail.com>
2024-06-07 17:57:16 +0300

    config(test): update Vitest setup file path
    - Changed setup file path from src/shared/config/test/setupTests.ts to
    src/shared/config/test/vitest.setup.ts

--- config/vite/vitest.ts
+++ config/vite/vitest.ts
@@ -4,7 +4,7 @@ import { configDefaults } from 'vitest/config'
 export const test = {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/shared/config/test/setupTests.ts',
+    setupFiles: 'src/shared/config/test/vitest.setup.ts',
     include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
     coverage: {
         reporter: ['text', 'lcov'],
