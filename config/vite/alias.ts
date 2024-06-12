aa9462f521b655c2d28dec3e482f4a903dd6670f
rtao-god <prostodimka519@gmail.com>
2024-06-12 12:51:05 +0300

    config(alias): update style module aliases
    Updated the style module aliases to use
    '/__mocks__/styleMock.js' for .module.scss
    and .module.sass files.

--- config/vite/alias.ts
+++ config/vite/alias.ts
@@ -3,6 +3,8 @@ import path from 'path'
 export const alias = {
     alias: {
         '@': path.resolve(__dirname, '../../src'),
-        '\\.module\\.(sass|scss)$': 'identity-obj-proxy'
+        // '\\.module\\.(sass|scss)$': 'identity-obj-proxy',
+        '\\.module\\.scss$': '/__mocks__/styleMock.js',
+        '\\.module\\.sass$': '/__mocks__/styleMock.js'
     }
 }
