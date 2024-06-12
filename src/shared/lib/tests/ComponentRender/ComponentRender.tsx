da6e1ef82de8f80acb825c5b4ba82e5d22b53a20
rtao-god <prostodimka519@gmail.com>
2024-06-12 13:12:32 +0300

    refactor(tests): update ComponentRender for better provider handling
    Refactored the ComponentRender
    to include new parameters
    and optimize the use
    of providers for testing. Introduced AllProviders component for cleaner composition
    and added customRender function to enhance testing capabilities.

--- src/shared/lib/tests/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender/ComponentRender.tsx
@@ -1,23 +1,36 @@
 import { MemoryRouter } from 'react-router-dom'
 import { ReactNode } from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderOptions } from '@testing-library/react'
 import { I18nextProvider } from 'react-i18next'
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider'
 import i18nForTests from '@/shared/config/i18n/i18nForTests'
 
 export interface ComponentRenderPropsOptions {
+    children?: ReactNode
     route?: string
     initialState?: DeepPartial<StateSchema>
 }
 
-export default function ComponentRender(component: ReactNode, options: ComponentRenderPropsOptions = {}) {
-    const { route = '/', initialState } = options
-
-    return render(
+const AllProviders: React.FC<ComponentRenderPropsOptions> = ({ children, route = '/', initialState }) => {
+    return (
         <StoreProvider initialState={initialState}>
             <MemoryRouter initialEntries={[route]}>
-                <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+                <I18nextProvider i18n={i18nForTests}>{children}</I18nextProvider>
             </MemoryRouter>
         </StoreProvider>
     )
 }
+
+const customRender = (ui: ReactNode, options?: Omit<RenderOptions, 'wrapper'> & ComponentRenderPropsOptions) => {
+    const { route, initialState, ...renderOptions } = options ?? {}
+
+    return render(
+        <AllProviders route={route} initialState={initialState}>
+            {ui}
+        </AllProviders>,
+        renderOptions
+    )
+}
+
+export * from '@testing-library/react'
+export { customRender as render }
